e911b2867eee91e02ba0fd91738c37002bca477e
gohar <gohardhuddi@gmail.com>
2020-11-25 11:09:55 -0800

    Making male and female cards clickable with changing color .

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -6,6 +6,7 @@ import 'reuseable_card.dart';
 
 const BottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1F33);
+const incativeCardColor = Color(0xFF181A2E);
 const BottomContainerColor = Color(0xFFE62E54);
 
 class InputPage extends StatefulWidget {
@@ -14,6 +15,25 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleCardColor = incativeCardColor;
+  Color femaleCardColor = incativeCardColor;
+  void updateGander(int gander) {
+    if (gander == 1) {
+      //male
+      if (maleCardColor == incativeCardColor) {
+        maleCardColor = activeCardColor;
+      } else {
+        maleCardColor = incativeCardColor;
+      }
+    } else {
+      if (femaleCardColor == incativeCardColor) {
+        femaleCardColor = activeCardColor;
+      } else {
+        femaleCardColor = incativeCardColor;
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,20 +47,34 @@ class _InputPageState extends State<InputPage> {
                 child: Row(
               children: [
                 Expanded(
-                  child: Reuseable_card(
-                    colour: activeCardColor,
-                    cardChild: icon_contant(
-                      icon: FontAwesomeIcons.male,
-                      label: 'MALE',
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateGander(1);
+                      });
+                    },
+                    child: Reuseable_card(
+                      colour: maleCardColor,
+                      cardChild: icon_contant(
+                        icon: FontAwesomeIcons.male,
+                        label: 'MALE',
+                      ),
                     ),
                   ),
                 ),
                 Expanded(
-                  child: Reuseable_card(
-                    colour: activeCardColor,
-                    cardChild: icon_contant(
-                      icon: FontAwesomeIcons.female,
-                      label: "FEMALE",
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateGander(0);
+                      });
+                    },
+                    child: Reuseable_card(
+                      colour: femaleCardColor,
+                      cardChild: icon_contant(
+                        icon: FontAwesomeIcons.female,
+                        label: "FEMALE",
+                      ),
                     ),
                   ),
                 ),
