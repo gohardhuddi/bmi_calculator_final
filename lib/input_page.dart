455f1a99e1a75c64d0c39613aaabe8c92a457480
gohar <gohardhuddi@gmail.com>
2020-11-25 06:03:52 -0800

    Create constractor name:Reuseable_card to accept perameters from all cards.

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -20,24 +20,34 @@ class _InputPageState extends State<InputPage> {
                 child: Row(
               children: [
                 Expanded(
-                  child: Reuseable_card(),
+                  child: Reuseable_card(
+                    colour: Color(0xFF1D1F33),
+                  ),
                 ),
                 Expanded(
-                  child: Reuseable_card(),
+                  child: Reuseable_card(
+                    colour: Color(0xFF1D1F33),
+                  ),
                 ),
               ],
             )),
             Expanded(
-              child: Reuseable_card(),
+              child: Reuseable_card(
+                colour: Color(0xFF1D1F33),
+              ),
             ),
             Expanded(
               child: Row(
                 children: [
                   Expanded(
-                    child: Reuseable_card(),
+                    child: Reuseable_card(
+                      colour: Color(0xFF1D1F33),
+                    ),
                   ),
                   Expanded(
-                    child: Reuseable_card(),
+                    child: Reuseable_card(
+                      colour: Color(0xFF1D1F33),
+                    ),
                   ),
                 ],
               ),
@@ -48,6 +58,8 @@ class _InputPageState extends State<InputPage> {
 }
 
 class Reuseable_card extends StatelessWidget {
+  Reuseable_card({@required this.colour});
+  final Color colour;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -55,7 +67,7 @@ class Reuseable_card extends StatelessWidget {
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10.0),
-        color: Color(0xFF1D1F33),
+        color: colour,
       ),
     );
   }
